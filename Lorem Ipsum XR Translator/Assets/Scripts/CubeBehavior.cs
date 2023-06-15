3d718e6bcadea91739f4915a96a827054d205aa3
Satto <satoshi.sugai.g@gmail.com>
2023-06-15 18:00:10 +0100

    Changed API call from HttpClient to UnityWebRequest and refactored.

--- Lorem Ipsum XR Translator/Assets/Scripts/CubeBehavior.cs
+++ Lorem Ipsum XR Translator/Assets/Scripts/CubeBehavior.cs
@@ -3,58 +3,42 @@ using System.Collections.Generic;
 using Microsoft.MixedReality.Toolkit.Input;
 using UnityEngine;
 using TMPro;
-using UnityEngine.Networking;
 
-// use it for sending requests to ChatGPT
-using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using System.Text;
-using Newtonsoft.Json;
+// our own namespace
+using Description;
 
 public class CubeBehavior : MonoBehaviour, IMixedRealityGestureHandler
 {
-    public TextMeshPro text;
+    public TextMeshPro textObject;
 
-    private ChatGPTClient _client;
+    private IDescriptionAPIClient _client;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Started");
-        text.text = "Sugai";
+        textObject.text = "Sugai";
 
         // TODO: Check your API KEY here: https://platform.openai.com/account/api-keys
-        this._client = new ChatGPTClient("PUT YOUR API KEY HERE", "text-davinci-003");
-        this.updateText("Good morning");
+        this._client = new ChatGPTClient("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", "text-davinci-003");
+        StartCoroutine(this._client.SendRequest("Good morning!", this.getDescription));
     }
-
-    // Update is called once per frame
-    //void Update()
-    //{
-    //   Debug.Log("Updated");
-    //}
 
     public void OnGestureStarted(InputEventData eventData)
     {
         Debug.Log("Gesture Started");
-        text.text = "Gesture Started";
-        this.updateText("Say hey");
+        StartCoroutine(this._client.SendRequest("Good afternoon!", this.getDescription));
     }
 
     public void OnGestureUpdated(InputEventData eventData)
-    {
+    {            
         Debug.Log("Gesture Updated");
-        text.text = "Gesture Updated";
-        this.updateText("Say hi");
     }
  
     public void OnGestureCompleted(InputEventData eventData)
     {
         Debug.Log("Gesture Completed");
-        text.text = "Gesture Completed";
-        this.updateText("Say yo");
+        StartCoroutine(this._client.SendRequest("Good evening!", this.getDescription));
     }
 
     public void OnGestureCanceled(InputEventData eventData)
@@ -62,94 +46,15 @@ public class CubeBehavior : MonoBehaviour, IMixedRealityGestureHandler
         Debug.Log("Gesture Canceled");
     }
 
-    public async void updateText(string greeting)
+    private void getDescription(string responseText)
     {
-        Debug.Log("Call ChatGPT API");
-        text.text = await this._client.SendPrompt(greeting);
-    }
-}
-
-          
-public class ChatGPTClient
-{ 
-    private readonly HttpClient _httpClient;
-    private readonly string _apiKey;
-    private readonly string _model;
-
-    public ChatGPTClient(string apiKey, string model)
-    {
-        this._httpClient = new HttpClient();
-        this._httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + apiKey);
-        this._model = model;
-    }
-
-    public async Task<string> SendPrompt(string prompt)
-    {
-        // JSON body data
-        var reqBody = new PromptRequest(this._model, prompt, 10, 0.0f);
-        string reqBodyJson = JsonUtility.ToJson(reqBody);
-
-        // Convert the body data to JSON and add to the request
-        HttpContent content = new StringContent(reqBodyJson, Encoding.UTF8, "application/json");
-        
-        HttpResponseMessage res = await this._httpClient.PostAsync("https://api.openai.com/v1/completions", content);
-        Debug.Log("ChatGPTClient: Got a response");
-
-        if (res.IsSuccessStatusCode)
+        if (responseText != null)
         {
-            Debug.Log("ChatGPTClient: Reading the response");
-            string resContent = await res.Content.ReadAsStringAsync();
-            Debug.Log("ChatGPTClient: Response Text: " + resContent);
-            return resContent;
+            textObject.text = responseText;
         }
         else
         {
-            Debug.Log("ChatGPTClient: Error Status Code received: " + res.StatusCode + ". Message: " + res.ReasonPhrase);
-            return "error";
+            Debug.Log("Got null. Must be something wrong with Description API client's implementation");
         }
     }
-}
-
-[Serializable]
-public class PromptRequest
-{
-    public string model;
-    public string prompt;
-    public int max_tokens;
-    public float temperature;
-
-    public PromptRequest(string model, string prompt, int max_tokens, float temperature)
-    {
-        this.model = model;
-        this.prompt = prompt;
-        this.max_tokens = max_tokens;
-        this.temperature = temperature;
-    }
-}
-
-[Serializable]
-public class Choice
-{
-    public string text;
-    public int index;
-    public string finish_reason;
-}
-
-[Serializable]
-public class Usage
-{
-    public int prompt_tokens;
-    public int completion_tokens;
-    public int total_tokens;
-}
-
-[Serializable]
-public class Response
-{
-    public string id;
-    public string object_type;
-    public int created;
-    public string model;
-    public List<Choice> choices;
-    public Usage usage;
 }
---
+++ Lorem Ipsum XR Translator/Assets/Scripts/Description/ChatGPTClient.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+namespace Description
+{
+    public class ChatGPTClient : IDescriptionAPIClient
+    {
+        private const string Url = "https://api.openai.com/v1/completions";
+        private readonly string _apiKey;
+        private readonly string _model;
+
+        public ChatGPTClient(string apiKey, string model)
+        {
+            this._apiKey = apiKey;
+            this._model = model;
+        }
+
+        public IEnumerator SendRequest(string content, Action<string> callback)
+        {
+            // Create a POST request with the specified URL.
+            UnityWebRequest www = new UnityWebRequest(ChatGPTClient.Url, UnityWebRequest.kHttpVerbPOST);
+
+            // Create body of the request. PromtpRequest -> json -> bytes
+            var reqBody = new PromptRequest(this._model, content, 10, 0.0f);
+            byte[] reqBodyRaw = Encoding.UTF8.GetBytes(JsonUtility.ToJson(reqBody));
+
+            // Set body of the request.
+            www.uploadHandler = new UploadHandlerRaw(reqBodyRaw);
+
+            // Set headers
+            www.SetRequestHeader("Content-Type", "application/json");
+            www.SetRequestHeader("Authorization", "Bearer " + this._apiKey);
+
+            www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+            // Send the request and wait for a response
+            yield return www.SendWebRequest();
+
+            // Got a response with Connection Error
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.LogError("Connection Error: " + www.error);
+                callback("Connection Error: " + www.error);
+            }
+            // Got a reponse with Protocol Error
+            else if (www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Protocol Error: " + www.error);
+                callback("Protocol Error: " + www.error);
+            }
+            // Got a response without any error
+            else
+            {
+                string message = "";
+                // response text is null
+                if (www.downloadHandler.text == null) message = "downloadHander.text is null.";
+                // response text is an empty string
+                else if (www.downloadHandler.text == "") message = "downloadHander.text is an empty string.";
+                // valid response
+                else
+                {
+                    // Parse the response in json into an object.
+                    // A response from ChatGPT API should look like what's defined below (Response class).
+                    Response res = JsonUtility.FromJson<Response>(www.downloadHandler.text);
+                    if (res == null) message = "response is empty.";
+                    else if (res.choices == null || res.choices.Count == 0) message = "choices is empty. Read the ChatGPT document for more details.";
+                    else message = res.choices[0].text;
+                }
+                Debug.Log(message);
+                callback(message);
+            }
+        }
+
+        // Request body.
+        [Serializable]
+        private class PromptRequest
+        {
+            public string model;
+            public string prompt;
+            public int max_tokens;
+            public float temperature;
+
+            public PromptRequest(string model, string prompt, int max_tokens, float temperature)
+            {
+                this.model = model;
+                this.prompt = prompt;
+                this.max_tokens = max_tokens;
+                this.temperature = temperature;
+            }
+        }
+
+        // Response body
+        [Serializable]
+        private class Response
+        {
+            public string id;
+            public string object_type;
+            public int created;
+            public string model;
+            public List<Choice> choices;
+            public Usage usage;
+        }
+
+        [Serializable]
+        private class Choice
+        {
+            public string text;
+            public int index;
+            public string finish_reason;
+        }
+
+        [Serializable]
+        private class Usage
+        {
+            public int prompt_tokens;
+            public int completion_tokens;
+            public int total_tokens;
+        }
+    }
+}
