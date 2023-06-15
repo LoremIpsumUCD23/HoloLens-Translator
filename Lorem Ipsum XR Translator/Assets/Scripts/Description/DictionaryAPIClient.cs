29398497b2e26e8c801f54f4cc213041879fbc6d
Satto <satoshi.sugai.g@gmail.com>
2023-06-15 18:08:56 +0100

    Implemented skeleton code for dictionary API

---
+++ Lorem Ipsum XR Translator/Assets/Scripts/Description/DictionaryAPIClient.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+namespace Description
+{
+    public class DictionaryAPIClient : IDescriptionAPIClient
+    {
+        public DictionaryAPIClient()
+        {
+            // TODO: complete this constructor
+        }
+
+        public IEnumerator SendRequest(string content, Action<string> callback)
+        {
+            // TODO: complete this method
+            // callback("Put a resulting string or error message here at the end of this method.");
+            return null;
+        }
+    }
+}
