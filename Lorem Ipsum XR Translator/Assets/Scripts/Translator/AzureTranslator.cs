574a16446672ed889eb8704a4fcb0e9593d0e0a8
Satto <satoshi.sugai.g@gmail.com>
2023-06-19 14:08:44 +0100

    Fixed the memory leak problem

--- Lorem Ipsum XR Translator/Assets/Scripts/Translator/AzureTranslator.cs
+++ Lorem Ipsum XR Translator/Assets/Scripts/Translator/AzureTranslator.cs
@@ -1,111 +1,72 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Text;
-using Newtonsoft.Json;
-using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.Networking;
-using System;
-using System.Net.Http;
+using Newtonsoft.Json;
 
 
 namespace Translator
 {
 
-public class AzureTranslator : ITranslatorClient
-{
-    //private static readonly string key = "5878a06b7c2c4a66beed0915fe52a400";
-    private readonly string endpoint = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&";
-
-    // location, also known as region.
-    // required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
-    //private static readonly string location = "northeurope";
-    private readonly string apiKey;
-    private readonly string  location;
-
-    public AzureTranslator(string apiKey,string location)
+    public class AzureTranslator : ITranslatorClient
     {
-        // private const string Url = "https://api.openai.com/v1/completions";
+        private const string endpoint = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&";
 
-        this.apiKey = apiKey;
-        this.location = location;
-        
-    }
+        // location, also known as region.
+        // required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
+        private readonly string apiKey;
+        private readonly string  location;
 
-
-    public IEnumerator Translate(string originalText, string from, string[] to, Action<string> callback)
-    {
-        string url = endpoint + string.Format("from={0}", from);
-        for (int i = 0; i < to.Length; i ++)
+        public AzureTranslator(string apiKey,string location)
         {
-            url += string.Format("&to={0}", to[i]);
+            this.apiKey = apiKey;
+            this.location = location;
         }
-        Debug.Log(url);
-
-        // Create the request body
-        string requestBody = "[{ \"Text\": \"" + originalText + "\" }]";
-        byte[] requestData = System.Text.Encoding.UTF8.GetBytes(requestBody);
-
-        
-    
-        // Create a UnityWebRequest object
-        UnityWebRequest request = UnityWebRequest.Post(url, requestBody);
-        request.uploadHandler = new UploadHandlerRaw(requestData);
-    
-        // Set the request headers
-        request.SetRequestHeader("Content-Type", "application/json");
-        
-
-        request.SetRequestHeader("Ocp-Apim-Subscription-Key", this.apiKey);
-        request.SetRequestHeader("Ocp-Apim-Subscription-Region",this.location); 
 
 
-        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-    
-        // Send the request
-        yield return request.SendWebRequest();
-    
-        // Handle the response
-        if (request.result != UnityWebRequest.Result.Success)
+        public IEnumerator Translate(string originalText, string from, string[] to, Action<string> callback)
         {
-            Debug.Log("Error: " + request.error);
-            callback("Error: " + request.error);
+            string url = AzureTranslator.endpoint + string.Format("from={0}", from);
+            for (int i = 0; i < to.Length; i ++)
+            {
+                url += string.Format("&to={0}", to[i]);
+            }
+
+            // Create the request body
+            string requestBody = "[{ \"Text\": \"" + originalText + "\" }]";
+            byte[] requestData = System.Text.Encoding.UTF8.GetBytes(requestBody);
+
+            // Send a request
+            using (UnityWebRequest request = UnityWebRequest.Post(url, requestBody))
+            {
+                request.uploadHandler = new UploadHandlerRaw(requestData);
+
+                // Set the request headers
+                request.SetRequestHeader("Content-Type", "application/json");
+                request.SetRequestHeader("Ocp-Apim-Subscription-Key", this.apiKey);
+                request.SetRequestHeader("Ocp-Apim-Subscription-Region", this.location);
+
+                request.downloadHandler = (DownloadHandler) new DownloadHandlerBuffer();
+
+                // Send the request
+                yield return request.SendWebRequest();
+
+                // Handle the response
+                if (request.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.Log("Error: " + request.error);
+                    callback("Error: " + request.error);
+                }
+                else
+                {
+                    string responseText = request.downloadHandler.text;
+                    // Parse and process the response as needed
+                    Debug.Log("Translation response: " + responseText);
+                    callback(responseText);
+                }
+            } 
         }
-        else
-        {
-            string responseText = request.downloadHandler.text;
-            // Parse and process the response as needed
-            Debug.Log("Translation response: " + responseText);
-            callback(responseText);
-        }
-    }
-
-    //public static async Task Main()
-    //{
-        // Input and output languages are defined as parameters.
-    //    string route = "/translate?api-version=3.0&from=en&to=lan1&to=ja";
-    //    string textToTranslate = "Hi , my name is tintin";
-         
-    //    object[] body = new object[] { new { Text = textToTranslate } };
-    //    var requestBody = JsonConvert.SerializeObject(body);
-
-    //    using (var client = new HttpClient())
-    //    using (var request = new HttpRequestMessage())
-    //    {
-            // Build the request.
-    //        request.Method = HttpMethod.Post;
-    //        request.RequestUri = new Uri(endpoint + route);
-    //        request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
-    //        request.Headers.Add("Ocp-Apim-Subscription-Key", key);
-            // location required if you're using a multi-service or regional (not global) resource.
-    //        request.Headers.Add("Ocp-Apim-Subscription-Region", location);
-
-            // Send the request and get response.
-    //        HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
-            // Read response as a string.
-    //        string result = await response.Content.ReadAsStringAsync();
-           // Console.WriteLine(result);
-    //        Debug.Log(result);
-    //    }
     }
 }
