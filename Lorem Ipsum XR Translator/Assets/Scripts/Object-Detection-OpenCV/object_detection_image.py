fcb308e389340836c6cdb14473c39df1d84c7098
MansiKAmbalam <mansi.ambalam@ucdconnect.ie>
2023-06-30 18:40:19 +0100

    Added support in custom model to return json response similar to azure for code reusability

--- Lorem Ipsum XR Translator/Assets/Scripts/Object-Detection-OpenCV/object_detection_image.py
+++ Lorem Ipsum XR Translator/Assets/Scripts/Object-Detection-OpenCV/object_detection_image.py
@@ -1,9 +1,23 @@
 import cv2
+import json
 
-image = cv2.imread('examples/image3.jpg')
+image_path = 'examples/desk.jpeg'
+image = cv2.imread(image_path)
 image = cv2.resize(image, (640, 480))
 h = image.shape[0]
 w = image.shape[1]
+
+response_dict = {}
+response_dict["requestId"] = "N/A"
+response_dict["modelVersion"] = "N/A"
+meta_dict = {
+        "height":h,
+        "width":w,
+        "format":"Jpeg"
+    }
+response_dict["metadata"] = meta_dict
+
+object_list = []
 
 # path to the weights and model files
 weights = "ssd_mobilenet/frozen_inference_graph.pb"
@@ -25,27 +39,43 @@ output = net.forward()  # shape: (1, 1, 100, 7)
 
 # loop over the number of detected objects
 for detection in output[0, 0, :, :]:  # output[0, 0, :, :] has a shape of: (100, 7)
+    obj_dict = {}
     # the confidence of the model regarding the detected object
     probability = detection[2]
+    obj_dict["confidence"] = float(detection[2])
 
     # if the confidence of the model is lower than 50%,
     # we do nothing (continue looping)
     if probability < 0.5:
         continue
-
+    #print(detection)
     # perform element-wise multiplication to get
     # the (x, y) coordinates of the bounding box
     box = [int(a * b) for a, b in zip(detection[3:7], [w, h, w, h])]
     box = tuple(box)
+    #print(box)
     # draw the bounding box of the object
     cv2.rectangle(image, box[:2], box[2:], (0, 255, 0), thickness=2)
-
+    rect_dict = {
+            "x": box[0],
+            "y": box[2],
+            "w": box[2]-box[0],
+            "h": box[3]-box[1],
+        }
     # extract the ID of the detected object to get its name
     class_id = int(detection[1])
     # draw the name of the predicted object along with the probability
     label = f"{class_names[class_id - 1].upper()} {probability * 100:.2f}%"
+    obj_dict["object"] = class_names[class_id - 1]
     cv2.putText(image, label, (box[0], box[1] + 15),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    obj_dict["rectangle"] = rect_dict
+    object_list.append(obj_dict)
 
-cv2.imshow('Image', image)
-cv2.waitKey()
+response_dict["objects"] = object_list
+
+response_text = json.dumps(response_dict)
+print(response_text)
+
+#cv2.imshow('Image', image)
+#cv2.waitKey()
