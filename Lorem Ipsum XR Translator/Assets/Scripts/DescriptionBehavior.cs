060f705a1c169af4d66318e3c02f6a34e78dc4ae
Peter-Choah <petercws22@gmail.com>
2023-07-05 22:27:22 +0100

    Created a dialog panel for description
    The dialog behaviour is not working yet, will need to do more testing tomorrow.

---
+++ Lorem Ipsum XR Translator/Assets/Scripts/DescriptionBehavior.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+using TMPro;
+
+// our own namespace
+using Description;
+using Translator;
+using Config;
+
+public class DescriptionBehavior : MonoBehaviour, IMixedRealityGestureHandler
+{
+    [SerializeField] private TextMeshPro TranslationText;
+    [SerializeField] private TextMeshPro DictionaryText;
+    [SerializeField] private TextMeshPro ChatGPTText;
+
+    private ITranslatorClient _translatorClient;
+    private IDescriptionClient _chatGPTClient;
+    private IDescriptionClient _dictionaryClient;
+
+
+    // for testing.
+    // private string[] objects = { "cup", "dog", "human", "rocket", "tree" };
+    [SerializeField] private TextMeshPro Title;
+
+    [SerializeField] private string word; // word to be described
+    [SerializeField] private string secondarylanguage; // chosen secondary language
+
+    void Start()
+    {
+        Debug.Log("Initialising");
+
+        word = "cube";
+        secondarylanguage = "fr";
+        Title.text = word;
+
+        // Initialise translator client
+        this._translatorClient = new AzureTranslator(Secrets.GetAzureTranslatorKey(), "northeurope");
+
+        // Initialise decription client
+        this._dictionaryClient = new DictionaryAPIClient("elementary", Secrets.GetDictApiKeyFor("elementary"));
+
+        // Initialise decription client
+        this._chatGPTClient = new ChatGPTClient(Secrets.GetChatGPTApiKey(), "text-davinci-003");
+    }
+
+    public void OnGestureStarted(InputEventData eventData)
+    {
+        Debug.Log("Gesture Started");
+
+        Title.text = word;
+        string target = word;
+        string originalLanguage = "en";
+        string[] targetLanguages = new string[] { "fr" };
+
+
+        // Initialise translator client
+        this._translatorClient = new AzureTranslator(Secrets.GetAzureTranslatorKey(), "northeurope");
+        // Get a translation in French.
+        StartCoroutine(this._translatorClient.Translate(target, originalLanguage, targetLanguages, this.GetTranslation));
+
+        // Initialise decription client
+        this._dictionaryClient = new DictionaryAPIClient("elementary", Secrets.GetDictApiKeyFor("elementary"));
+        // Get a description.
+        StartCoroutine(this._dictionaryClient.SendRequest(target, this.GetDescriptionFromDict));
+
+        // Initialise decription client
+        this._chatGPTClient = new ChatGPTClient(Secrets.GetChatGPTApiKey(), "text-davinci-003");
+        // Get a description.
+        string prompt = "Definition of " + target;
+        StartCoroutine(this._chatGPTClient.SendRequest(prompt, this.GetDescriptionFromGPT));
+
+    }
+
+    public void OnGestureUpdated(InputEventData eventData)
+    {
+        Debug.Log("Gesture Updated");
+    }
+
+    public void OnGestureCompleted(InputEventData eventData)
+    {
+        Debug.Log("Gesture Completed");
+    }
+
+    public void OnGestureCanceled(InputEventData eventData)
+    {
+        Debug.Log("Gesture Canceled");
+    }
+
+    private void GetTranslation(string responseText)
+    {
+        if (responseText != null)
+        {
+            TranslationText.text = "Translator: " + responseText;
+        }
+        else
+        {
+            Debug.Log("Got null. Must be something wrong with Translation API client's implementation");
+        }
+    }
+
+    private void GetDescriptionFromDict(string responseText)
+    {
+        if (responseText != null)
+        {
+            DictionaryText.text = "Dict: " + responseText;
+        }
+        else
+        {
+            Debug.Log("Got null. Must be something wrong with Description API client's implementation");
+        }
+    }
+
+    private void GetDescriptionFromGPT(string responseText)
+    {
+        if (responseText != null)
+        {
+            ChatGPTText.text = "GPT: " + responseText;
+        }
+        else
+        {
+            Debug.Log("Got null. Must be something wrong with Description API client's implementation");
+        }
+    }
+}
