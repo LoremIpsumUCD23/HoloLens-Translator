f24a97573f85ab8be90217d1c9510d4e57c40620
Vijayditya Sarker <vijayditya.sarker@ucdconnect.ie>
2023-07-28 13:50:18 +0100

    Added comments to SpeechTotextDisplay Script

--- Lorem Ipsum XR Translator/Assets/Scripts/SpeechToText/SpeechToTextDisplay.cs
+++ Lorem Ipsum XR Translator/Assets/Scripts/SpeechToText/SpeechToTextDisplay.cs
@@ -6,18 +6,34 @@ using Config;
 
 namespace STT
 {
+    /// <summary>
+    /// The class SpeechToTextDisplay is defined within the STT namespace is used to translate and display in the scene 
+    /// </summary>
     public class SpeechToTextDisplay : MonoBehaviour
     {
+        /// <summary>
+        /// Calls the AzureSpeechToText script
+        /// </summary>
         [SerializeField]
         private AzureSpeechToText azureSpeechToText; 
 
+        
+        /// <summary>
+        /// originalToolTip is used to display the text converted from Speech to text
+        /// </summary>
         [SerializeField]
         private ToolTip originalToolTip; 
 
+        
+        /// <summary>
+        /// translatedToolTip is used to display the translated text
+        /// </summary>
         [SerializeField]
         private ToolTip translatedToolTip; 
 
-        // Azure Translator setup
+        /// <summary>
+        /// Azure Translator setup
+        /// </summary>
         private AzureTranslator azureTranslator;
         private string apiKey;  
         private const string location = "northeurope";
@@ -29,40 +45,59 @@ namespace STT
             azureTranslator = new AzureTranslator(apiKey, location);
         }
 
+        
+        /// <summary>
+        /// This method is called before the first frame update in Unity. It checks if the tooltips are assigned and sets them to be inactive (hidden) initially.
+        /// </summary>
         private void Start()
-{
-    if (originalToolTip != null)
-    {
-        originalToolTip.gameObject.SetActive(false);
-    }
-    else
-    {
-        Debug.LogWarning("Original ToolTip is not assigned.");
-    }
+        {
+            if (originalToolTip != null)
+            {
+                originalToolTip.gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.LogWarning("Original ToolTip is not assigned.");
+            }
 
-    if (translatedToolTip != null)
-    {
-        translatedToolTip.gameObject.SetActive(false);
-    }
-    else
-    {
-        Debug.LogWarning("Translated ToolTip is not assigned.");
-    }
-}
+            if (translatedToolTip != null)
+            {
+                translatedToolTip.gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.LogWarning("Translated ToolTip is not assigned.");
+            }
+        }
 
-
+         /// <summary>
+        /// TranslateAndDisplay which is a asynchronous method is called when a user action (button press) occurs.
+        /// It requests Azure's Speech to Text service and translation service to recognize speech and then displays the recognized and translated text in the tooltips.
+        /// </summary>
         public async void OnRequestSpeechToText()
         {
-            // Call the SSTRequest method and get the recognized text or error message
+            
+            /// <summary>
+            /// Call the SSTRequest method and get the recognized text or error message
+            /// </summary>
             string recognizedText = await azureSpeechToText.SSTRequest();
 
-            // Display the recognized text in the original ToolTip
+            
+            /// <summary>
+            /// Display the recognized text in the original ToolTip
+            /// </summary>
             DisplayInToolTip(originalToolTip, recognizedText);
 
-            // Translate and display in the translated ToolTip
+            /// <summary>
+            /// Translate and display in the translated ToolTip
+            /// </summary>
             StartCoroutine(TranslateAndDisplay(recognizedText));
         }
 
+        
+        /// <summary>
+        /// This method is a coroutine that takes the recognized text, sends it for translation, and then displays the translated text in the tooltip.
+        /// </summary>
         private IEnumerator TranslateAndDisplay(string text)
         {
             yield return azureTranslator.Translate(text, "en", new string[] { targetLanguage }, response =>
@@ -70,12 +105,19 @@ namespace STT
                 // Deserialize the root array
                 TranslationResponse[] translationResponses = JsonUtility.FromJson<Wrapper>("{\"array\":" + response + "}").array;
 
-                // Check if we got any responses
+                
+                /// <summary>
+                /// Checks if we got any responses
+                /// </summary>
+                
                 if (translationResponses != null && translationResponses.Length > 0)
                 {
                     TranslationResponse translationResponse = translationResponses[0];
 
-                    // Check if the translationResponse contains translations
+                    
+                    /// <summary>
+                    /// Checks if the translationResponse contains translations
+                     /// </summary>
                     if (translationResponse.translations != null && translationResponse.translations.Length > 0)
                     {
                         string translatedText = translationResponse.translations[0].text;
@@ -93,6 +135,9 @@ namespace STT
             });
         }
 
+         /// <summary>
+        /// DisplayInToolTip method sets the provided text to the given tooltip and makes the tooltip visible
+        /// </summary>
         private void DisplayInToolTip(ToolTip toolTip, string text)
         {
             if (toolTip != null)
@@ -106,7 +151,10 @@ namespace STT
             }
         }
 
-
+         /// <summary>
+        ///TranslationResponse,TranslationItem and Wrapper these are serializable helper classes used to deserialize the JSON response from the Azure Translator service.
+        /// It requests Azure's Speech to Text service and translation service to recognize speech and then displays the recognized and translated text in the tooltips.
+        /// </summary>
         [System.Serializable]
         public class TranslationResponse
         {
